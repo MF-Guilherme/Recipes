6c7f1be4de2075a9eeb26694c715d26263330b64
Gui Montenegro <guimontenegro23@yahoo.com.br>
2024-06-26 11:43:51 -0300

    feat: url, template e view para visualizar uma única receita

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('sobre/', views.sobre),
-    path('contato/', views.contato),
+    path('recipes/<int:id>/', views.recipe),
+
 ]
