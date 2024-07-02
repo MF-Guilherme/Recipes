9a27f38a1f40a730c4b57e1cb2ef1ef59e719ef1
Gui Montenegro <guimontenegro23@yahoo.com.br>
2024-07-02 16:46:49 -0300

    feat: criada view para exibir receitas de uma determinada categoria

--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from utils.recipes.factory import make_recipe
+from .models import *
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', {
-        'recipes': [make_recipe() for _ in range(10)]
-    })
+    recipes = Recipe.objects.all().order_by('-id')
+    return render(request, 'recipes/pages/home.html', {'recipes': recipes})
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id)
+    return render(request, 'recipes/pages/home.html', {'recipes': recipes})
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.get(id=id)
     return render(request, 'recipes/pages/recipe-view.html', {
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
     
