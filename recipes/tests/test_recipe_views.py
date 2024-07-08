2b6adae3a71126b00df131d32a76d51ecc3ef7c0
Gui Montenegro <guimontenegro23@yahoo.com.br>
2024-07-08 14:51:08 -0300

    test: testando content e context utilizando fixtures

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 from recipes import views
+from recipes.models import Category, Recipe, User
 
 
 class RecipeViewsTest(TestCase):
@@ -23,6 +24,39 @@ class RecipeViewsTest(TestCase):
             response.content.decode('utf-8'),
             )
 
+    def test_recipe_home_template_loads_recipes(self):
+        category = Category.objects.create(name='Massas')
+        author = User.objects.create_user(
+            first_name='user',
+            last_name='name',
+            username='username',
+            password='123456',
+            email='username@email.com',
+        )
+        recipe = Recipe.objects.create(
+            category=category,
+            author=author,
+            title='Recipe Title',
+            description='Recipe Description',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unit='Minutos',
+            servings=5,
+            servings_unit='Porções',
+            preparation_steps='Recipe Preparation Steps',
+            preparation_steps_is_html=False,
+            is_published=True,
+        )
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')  # converte o conteúdo para uma única string # noqa
+        response_context_recipes = response.context['recipes']
+
+        self.assertIn('Recipe Title', content)
+        self.assertIn('10 Minutos', content)
+        self.assertIn('5 Porções', content)
+        # self.assertIn(response.context['recipes'].first().author.first_name, content) # só aparecerá se tiver nome e sobrenome cadastrados # noqa
+        self.assertEqual(len(response_context_recipes), 1)
+
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
         self.assertIs(view.func, views.category)
@@ -42,4 +76,3 @@ class RecipeViewsTest(TestCase):
             reverse('recipes:recipe', kwargs={'id': 1000})
             )
         self.assertEqual(response.status_code, 404)
-
