8a2b4e597d0208a96c764f276168a398ee2854f2
Gui Montenegro <guimontenegro23@yahoo.com.br>
2024-07-10 18:24:22 -0300

    test: testando os campos default=False da Recipe

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -1,4 +1,4 @@
-from .test_recipe_base import RecipeTestBase
+from .test_recipe_base import RecipeTestBase, Recipe
 from parameterized import parameterized
 from django.core.exceptions import ValidationError
 
@@ -7,6 +7,21 @@ class RecipeModelTest(RecipeTestBase):
     def setUp(self) -> None:
         self.recipe = self.make_recipe()
         return super().setUp()
+
+    def make_recipe_without_defaults(self):
+        recipe = Recipe(
+            category=self.make_category(name='Test Default Category'),
+            author=self.make_author(username='newUser'),
+            title='Recipe Title',
+            description='Recipe Description',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unit='Minutos',
+            servings=5,
+            servings_unit='Porções',
+            preparation_steps='Recipe Preparation Steps',
+        )
+        return recipe
 
     @parameterized.expand([
         ('title', 65),
@@ -18,3 +33,25 @@ class RecipeModelTest(RecipeTestBase):
         setattr(self.recipe, field, 'A' * (max_length + 1))
         with self.assertRaises(ValidationError):
             self.recipe.full_clean()
+
+    def test_recipe_preparation_steps_is_html_is_false_by_default(self):
+        '''Needs a recipe without fields that have 'default' attribute
+        in this case 'preparation_steps_is_html' and 'is_published' '''
+        recipe = self.make_recipe_without_defaults()
+        recipe.full_clean()
+        recipe.save()
+        self.assertFalse(
+            recipe.preparation_steps_is_html,
+            msg='Recipe preparation_steps_is_html is not False'
+        )
+
+    def test_recipe_is_published_is_false_by_default(self):
+        '''Needs a recipe without fields that have 'default' attribute
+        in this case 'preparation_steps_is_html' and 'is_published' '''
+        recipe = self.make_recipe_without_defaults()
+        recipe.full_clean()
+        recipe.save()
+        self.assertFalse(
+            recipe.is_published,
+            msg='Recipe is_published is not False'
+        )
