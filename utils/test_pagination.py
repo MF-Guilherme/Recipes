65fa73bcfd3a0dc40a3afed9c4df1fd10790e75f
Gui Montenegro <guimontenegro23@yahoo.com.br>
2024-08-06 20:17:55 -0300

    test: teste para verificar se o meio do range está correto (alterando o início e final do range exibido)

--- utils/test_pagination.py
+++ utils/test_pagination.py
@@ -10,3 +10,20 @@ class PaginationTest(TestCase):
             current_page=1
         )
         self.assertEqual([1, 2, 3, 4], pagination)
+
+    def test_make_sure_middle_ranges_are_correct(self):  # noqa: E501
+        # Current Page = 10 | Qty Page = 2 | Middle Page = 10
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=10
+        )
+        self.assertEqual([9, 10, 11, 12], pagination)
+
+        # Current Page = 44 | Qty Page = 2 | Middle Page = 44
+        pagination = make_pagination_range(
+            page_range=list(range(1, 51)),
+            qty_pages=4,
+            current_page=44
+        )
+        self.assertEqual([43, 44, 45, 46], pagination)
